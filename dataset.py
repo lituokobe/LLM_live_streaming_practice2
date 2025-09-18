dd14e309066a09ef0a39f6d15534e238a81a1125
lituokobe <lituolituolituo@gmail.com>
2025-09-18 22:27:51 +0800

    pretrain and dataset preparation

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import json
 
+import torch
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -24,4 +25,22 @@ class PretrainDataset(Dataset):
 
     def __getitem__(self, index):
         sample = self.samples[index]
-        #
+        # We need to prepare Y as the next token of X, for generative training
+        # Y is one token ahead of X
+        self.tokenizer(
+            # assume each line is json format and includes key of "text"
+            str(sample['text']),
+            max_length = self.max_length,
+            padding = 'max_length',
+            truncation = True,
+            return_tensors = 'pt'
+        )
+        input_ids = encoding.input_ids.squeeze()
+        # mask out tokens that is not real content, we will ignore them in loss calculation
+        loss_mask = (input_ids != self.tokenizer.pad_token_id)
+
+        # X won't inlcude the last token, Y won't include the first token
+        X = torch.tensor(input_ids[:-1], dtype = torch.long)
+        Y = torch.tensor(input_ids[1:], dtype = torch.long)
+        loss_mask = torch.tensor(loss_mask[1:], dtype = torch.long)
+        return X, Y, loss_mask
--- pretrain.py
+++ pretrain.py
@@ -13,3 +13,47 @@ from contextlib import nullcontext
 from transformers import AutoTokenizer
 from model import MyModelConfig, MyModelForCausalLM
 from dataset import PretrainDataset
+
+warnings.filterwarnings("ignore")
+
+def Logger(content):
+    # if not distributed training, print on the single machine
+    # if distributed training, print on the master machine
+    if not ddp or dist.get_rank() ==0:
+    print(content)
+
+def get_lr(current_step, total_steps, lr):
+    """
+    function to adjust learning rate in the training. We use cos scheduler.
+    Args:
+        - current_step: current iteration
+        - total_steps: total iteration neede
+        - lr: initial learning rate
+    """
+    return lr/10 + 0.5*lr*(1+math.cos(math.pi*current_step/total_steps))
+
+def init_model(lm_config):
+    # read a readily available tokenizer from huggingface
+    tokenizer = AutoTokenizer.from_pretrained('./model')
+    # use our own class to initialize a model
+    model = MyModelForCausalLM(lm_config).to(args.device)
+    Logger(f"Trainable parameters in the model: {sum(p.numel() for p in model.parameters() if p.requires_grad)/1e6:.2f} millions.")
+    return model, tokenizer
+
+def init_distributed_model(args):
+    if not ddp: return
+
+    global ddp_local_rank, DEVICE # maek them a global variable
+
+    dist.init_process_group(backend="nccl")
+    ddp_rank = int(os.environ["RANK"])
+    ddp_local_rank = int(os.environ["LOCAL_RANK"])
+    ddp_world_size = int(os.environ["WORLD_SIZE"])
+    # make the code run on the local device, meaning a specific GPU (local rank) on a specific machine (global rank)
+    DEVICE = f"cuda:{ddp_local_rank}"
+    torch.cuda.set_device(DEVICE)
+def train_epoch(epoch):
+
+
+
+if __name__ == "__main__":
